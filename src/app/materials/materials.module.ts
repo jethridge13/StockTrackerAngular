3721369304d5083df726bc09e584070a015beecb
Josh Ethridge <joshua.ethridge13@gmail.com>
2019-03-26 22:05:01 -0400

    Add toolbar with account button

--- src/app/materials/materials.module.ts
+++ src/app/materials/materials.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
+import {
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [],
   imports: [
+      MatButtonModule,
+      MatIconModule,
+    MatToolbarModule
+  ],
+  exports: [
+      MatButtonModule,
+      MatIconModule,
+    MatToolbarModule
   ]
 })
 export class MaterialsModule { }
