90f4885c7632863318ae59091122616eb55a4bb7
Josh Ethridge <joshua.ethridge13@gmail.com>
2019-04-16 19:33:01 -0400

    Start work on fleshing out stock card

--- src/app/stock-card/stock-card.component.ts
+++ src/app/stock-card/stock-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-stock-card',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StockCardComponent implements OnInit {
 
+  @Input() title = '';
+  @Input() subtitle = '';
+
+  showDelete = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  showDeletePrompt(): void {
+    this.showDelete = true;
   }
 
 }
